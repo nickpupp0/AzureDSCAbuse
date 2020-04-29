7e7ccd6d2b6392ff84577212257492704bfe3d23
nickpupp0 <nppuppo@gmail.com>
2020-04-29 16:40:52 -0400

    Delete push_reverse_shell_config.ps1

--- push_reverse_shell_config.ps1
+++
@@ -1,6 +0,0 @@
-$resourceGroup = ' '
-$vmName = ' '
-$storageName = ' '
-
-Publish-AzVMDscConfiguration -ConfigurationPath <path to config file> -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force
-Set-AzVMDscExtension -Version '2.80' -ResourceGroupName $resourceGroup -VMName $vmName -ArchiveStorageAccountName $storageName -ArchiveBlobName 'scheduled_task_config.ps1.zip' -AutoUpdate -ConfigurationName 'ReverseShellConfig'
